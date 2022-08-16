17f20b20d307341fe6b113697f0dbd0b96cdbbd1
siliziwe <siliziwepani@gmail.com>
2022-08-16 16:40:30 +0200

    It is what it is

--- index.js
+++ index.js
@@ -270,35 +270,33 @@ async (req, res) => {
 
 
 
-
-
 // Cart
 app.get("/users/:id/cart", (req, res) => {
     let sql = `SELECT cart FROM users WHERE user_id =${req.params.id}`;
     db.query(sql, (err, results) => {
-      if (err) throw err;
-      res.json({
+    if (err) throw err;
+    res.json({
         status: 200,
         results: JSON.parse(results[0].cart),
-      });
     });
-  });
+    });
+});
 
-  app.post("/users/:id/cart", bodyParser.json(), (req, res) => {
+app.post("/users/:id/cart", bodyParser.json(), (req, res) => {
     let bd = req.body;
     let sql = `SELECT cart FROM users WHERE user_id = ${req.params.id}`;
     db.query(
-      sql,
-      (err, results) => {
+    sql,
+    (err, results) => {
         if (err) throw err;
         if (results.length > 0) {
-          let cart;
-          if (results[0].length == null) {
+        let cart;
+        if (results[0].length == null) {
             cart = [];
-          } else {
+        } else {
             cart = JSON.parse(results[0].cart);
-          }
-          let product = {
+        }
+        let product = {
             "product_id": cart.length + 1,
             "title": bd.title,
             "category": bd.category,
@@ -307,19 +305,19 @@ app.get("/users/:id/cart", (req, res) => {
             "price": bd.price,
             "quantity": bd.quantity,
             "created_by": bd.created_by
-          };
+        };
 
-          cart.push(product);
-          let sql1 = `UPDATE users SET cart = ? WHERE user_id = ${req.params.id}`;
+        cart.push(product);
+        let sql1 = `UPDATE users SET cart = ? WHERE user_id = ${req.params.id}`;
 
-          db.query(sql1, JSON.stringify(cart), (err, results) => {
+        db.query(sql1, JSON.stringify(cart), (err, results) => {
             if (err) throw results;
             res.send(`Product add to your cart`);
-          });
+        });
         }
-      }
+    }
     );
-  });
+});
 
 
 app.delete("users/:id/cart", bodyParser.json(), (req, res) => {
